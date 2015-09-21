339bf8ea1e5e074f511cd2c8d37a05176714a1cd
rickisen <rickard.spam@gmail.com>
2015-09-21 11:02:53 +0200

    upg 3 klar

---
+++ html-css/upg2/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+		//$(".tableRow").css("bottom","202px").animate({ top: "+=202", }, 1000);
+		//$("#colorBoxDiv").css("height","100px");
+		//$("#colorBoxDiv").animate({ height: "+=202",},1000);
+		//$("#main").css("top","-200px").animate({top: "+=200",},1000);
+
+		$(document).scroll(function(){ $('#title').slideUp(); });
+});
